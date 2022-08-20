330e82537edc0d76cfe75326d445d4015548977f
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-08-20 16:17:59 +0530

    [Elavarasu] creating auth user model

--- user/models.py
+++ user/models.py
@@ -1,9 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
+class User(AbstractUser):
     phone = models.BigIntegerField()
     location = models.CharField(max_length=100)
