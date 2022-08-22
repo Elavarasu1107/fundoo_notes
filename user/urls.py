bc689675dfac72221aa2c037056376f05fb1b2ec
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-08-22 16:41:33 +0530

    [Elavarasu] Serializer for User Model

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('register', views.user_registration, name='register'),
-    path('login', views.user_login, name='login'),
+    path('register/', views.UserRegistration.as_view(), name='register'),
+    path('login/', views.UserLogin.as_view(), name='login'),
 ]
