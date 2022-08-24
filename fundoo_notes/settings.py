bcbd4f381d2b8dab4ca268f02efdd4e945a7e8f0
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-08-24 16:47:16 +0530

    [Elavarasu] Created Note model and views

--- fundoo_notes/settings.py
+++ fundoo_notes/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user.apps.UserConfig',
+    'notes.apps.NotesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ notes/tests/test_notes.py
@@ -0,0 +1,4 @@
+
+def test_check():
+    a = 20
+    assert a == 20
