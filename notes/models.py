64144ea45db63591676dedb06491a0fb6eaa8e7f
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-09-13 18:30:00 +0530

    [Elavarasu] Created Label model for notes

--- notes/models.py
+++ notes/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from user.models import User
 
 
+class Labels(models.Model):
+    title = models.TextField()
+    color = models.TextField()
+    font = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
 class Notes(models.Model):
     title = models.TextField()
     description = models.TextField()
     color = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     collaborator = models.ManyToManyField(User, related_name='collaborator')
+    label = models.ManyToManyField(Labels, related_name='label')
--- notes/urls.py
+++ notes/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('note/', views.Note.as_view(), name='note'),
     path('collaborator/', views.NoteCollaborator.as_view(), name='collaborator'),
+    path('label/', views.Label.as_view(), name='label'),
+    path('label_note/', views.AddLabelToNote.as_view(), name='label_note'),
 ]
